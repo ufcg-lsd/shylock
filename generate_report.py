236f6bc816bfc6cf444a952e21e4e3b29132c0ec
danielx285 <daniel.carlos.melo@ccc.ufcg.edu.br>
2020-04-30 02:52:40 -0300

    Falta comentar bastante coisa, mas há semelhanças entre a estrutura do primeiro e do segundo script

--- generate_report.py
+++ generate_report.py
@@ -20,16 +20,31 @@ def format_instance_log(log):
 	#here the result  indices are [Action, Request_ID, Message, Start_Time, Update_Time]
 	return [line.replace("|", " ").split() for line in log.split("\n")[3:-1]]
 
-print(format_instance_log(data["Default"]["admin"]["Instances"]["969630a1-8f3d-4504-b634-bbecc564b8cf"]["Log"]))
 
-logs_lsd = {}
+'''this code snippet is where we access all subdivisions of the cloud first we accesses
+the domains data["<Domain_Name>"], the value of each key is another dictionary, in this
+time the values of keys are dictionarys represeting projects ex. data["<Domain_Name>"]["<Project_Name>"]
+and so on as in the file get_data.py.
+example of use:
+	format_instance_log(data["Default"]["admin"]["Instances"]["969630a1-8f3d-4504-b634-bbecc564b8cf"]["Log"])
+'''
+for domain_name in data:
 
-for domain in data.values():
+	domain = data[domain_name]
 	
 	for project in domain.values():
+		print(project["Volume"])
+		
+		temporary_report = open("domains/%s/%s/Relatorio_%s.txt" % (domain_name, project["Name"], project["Name"]),  "w")
+		temporary_report.write("Volumes:\n")
+		for volume in project["Volume"]:
+			temporary_report.write("%s %sGb\n" % (volume["Name"], volume["Size"])) 
+					
+			#temporary_report.write(project["Volume"])
+		temporary_report.close()
 		for instance in project["Instances"].values():
 			for line in format_instance_log(instance["Log"]):
-				logs_lsd[line[0]] = "new"
+				pass	
+				
 
-print(logs_lsd.keys())
 	

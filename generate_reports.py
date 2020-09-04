faa7ff64bb09c441c9e98ccbe8fe4210db24bac9
Daniel Melo <38673841+danielx285@users.noreply.github.com>
2020-09-04 04:16:22 -0300

    Revert "add: report sumarizado para sponsors (#45)"
    This reverts commit 9ce7af00f24d39505ba4fc714fba55095eb2d444.

--- generate_reports.py
+++ generate_reports.py
@@ -3,7 +3,7 @@
 import jinja2, json, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--option", help="Select your report type(default_summary, to_sponsors_summary or default_reports separeted by ,)")
+parser.add_argument("--option", help="Select your report type(summary or default or both separeted by ,)")
 parser.add_argument("--date", help="Enter the start date")
 args = parser.parse_args()
 option = args.option.split(",")
@@ -11,15 +11,15 @@ option = args.option.split(",")
 with open("processed_data.json", "r") as json_file:
     processed_data = json.load(json_file)
 
-if "default_summary" in option:
-    with open("templates/default_summary_report_template.html") as template:
+if "summary" in option:
+    with open("templates/summary_report_template.html") as template:
         template = template.read()
     report = jinja2.Template(template).render(data = processed_data, date = args.date)
     
-    with open("reports/default_summary_report.html", "w") as html_file:
+    with open("reports/summary_report.html", "w") as html_file:
         html_file.write(report)
 
-if "default_reports" in option:
+if "default" in option:
     with open("templates/default_report_template.html") as template:
         template = template.read()
     
@@ -29,12 +29,4 @@ if "default_reports" in option:
     
         with open("reports/%s.html" % (sponsor), "w") as html_file:
             html_file.write(report)
-
-if "to_sponsors_summary" in option:
-    with open("templates/to_sponsors_summary_report_template.html") as template:
-        template = template.read()
-    report = jinja2.Template(template).render(data = processed_data, date = args.date)
-    
-    with open("reports/to_sponsors_summary_report.html", "w") as html_file:
-        html_file.write(report)
-    
+            
